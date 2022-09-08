479ef8b840319f72e6e4eab9ce4b77de0ddd5123
Ottavio Buonomo <ottavio@iPro-di-Ottavio.sunrise.box>
2022-09-08 15:16:48 +0200

    Initial commit

---
+++ Raver/Raver/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Raver
+//
+//  Created by Ottavio Buonomo on 08.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Raver!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Raver/Raver/RaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaverApp.swift
+//  Raver
+//
+//  Created by Ottavio Buonomo on 08.09.22.
+//
+
+import SwiftUI
+
+@main
+struct RaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
